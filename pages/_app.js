1248eb7b9526ac8f4b9e59e7a661cc56817eb139
Alam <amenabeato@gmail.com>
2022-09-02 01:26:57 -0400

    responsive sidebar added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import SideBar from "../components/SideBar/Index";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="flex flex-col">
+      <SideBar />
+      <div className="md:ml-72">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
